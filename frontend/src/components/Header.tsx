2ebc65ab6c841fa31c28ed8df4ffb61f029d029e
Amy <amaule65@gmail.com>
2024-05-07 15:50:38 +0200

    Create header

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <div className="header-container">    
+      <h1 className="header-title-container">
+        <span className="header-title-top">Let's answer some of society's</span>
+        <span className="header-title-bottom">most divisive questions</span>
+      </h1>
+      <h2 className="header-subtitle">Time to get some closure on some long-running debates.</h2>
+      <h2 className="header-subtitle">Vote below and see if others feel the same way you do</h2>
+    </div>
+  )
+}
+
+export default Header;
